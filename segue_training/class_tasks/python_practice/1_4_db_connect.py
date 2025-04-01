1d459b20923d57b991975a646d57fa954e264628
Lakshan S J <sj.lakshan2004@gmail.com>
2025-04-01 18:07:22 +0530

    01-04-25

---
+++ segue_training/class_tasks/python_practice/1_4_db_connect.py
@@ -0,0 +1,49 @@
+# import pyodbc
+# import datetime
+
+# conn = pyodbc.connect('DRIVER = (SQLSERVER); SERVER =;DATABASE = myproject;UID=HP_PAVILION\SQLEXPRESS01;PWD=')
+# cursor = conn.execute
+
+# questions = [
+
+import pyodbc
+
+
+conn = pyodbc.connect(
+    r"DRIVER={ODBC Driver 17 for SQL Server};"
+    r"SERVER=HP_PAVILION\SQLEXPRESS01;" 
+    r"DATABASE=myproject;"  
+    r"Trusted_Connection=yes;"
+)
+
+
+cursor = conn.cursor()
+
+cursor.execute("""
+    INSERT INTO Quiz_Questions (question, option_a, option_b, option_c, option_d, correct_option)
+    VALUES (?, ?, ?, ?, ?, ?)
+""", ("What is the correct extension of python?", ".python", ".pyx", ".py", ".program", "C"))
+
+
+cursor.execute("""
+    INSERT INTO Player_Score (player_name, score)
+    VALUES (?, ?)
+""", ("Harish", 50))
+
+# Insert data into the Execution_Logs table
+cursor.execute("""
+    INSERT INTO Execution_Logs (event_description, timestamp)
+    VALUES (?, ?)
+""", ("New record inserted into tables", "2025-04-01 12:00:00"))
+
+# Commit the transactions
+conn.commit()
+
+print("Values inserted successfully!")
+
+# Close the connection
+conn.close()
+
+
+
+
---
+++ segue_training/class_tasks/python_practice/1_4_one.py
@@ -0,0 +1,25 @@
+# print("Hello my new World!!!")
+
+n = input()  
+if n.isdigit():  
+    print("int")
+else:
+    print("str")
+
+
+a = 102
+b = 154
+c = float(a/b)
+print(c)
+
+num1 = 3 + 4j
+num2 = 2 + 5j
+print("Sum of Complex Numbers:", num1 + num2)
+
+
+def tycoon():
+    return None 
+result = tycoon()
+print("output:", result)
+
+
